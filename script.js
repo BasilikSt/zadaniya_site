672ade8f28517d345d930f254957a9bc7b24c3c4
BasilikSt <not.maya.st@gmail.com>
2026-01-07 15:50:27 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,23 @@
-fetch('tasks')
-  .then(() => {
-    fetchTasks();
+fetch('tasks.json')
+  .then(response => response.json())
+  .then(data => {
+    const container = document.getElementById('tasks');
+    container.innerHTML = '';
+
+    const tasks = data.tasks || data;
+
+    tasks.sort((a, b) => new Date(a.date) - new Date(b.date));
+
+    tasks.forEach(task => {
+      const div = document.createElement('div');
+      div.className = 'task';
+
+      div.innerHTML = `
+        <div class="task-date">${task.date}</div>
+        <div class="task-title">${task.title}</div>
+        <a href="${task.file}" download>Скачать</a>
+      `;
+
+      container.appendChild(div);
+    });
   });
-
-async function fetchTasks() {
-  const response = await fetch('tasks/index.json');
-  const tasks = await response.json();
-
-  const container = document.getElementById('tasks');
-  container.innerHTML = '';
-
-  tasks.sort((a, b) => new Date(a.date) - new Date(b.date));
-
-  tasks.forEach(task => {
-    const div = document.createElement('div');
-    div.className = 'task';
-
-    div.innerHTML = `
-      <div class="task-date">${task.date}</div>
-      <div class="task-title">${task.title}</div>
-      <a href="${task.file}" download>Скачать</a>
-    `;
-
-    container.appendChild(div);
-  });
-}
